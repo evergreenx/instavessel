d1f41096db60457c9756ba58df84f712441edcad
ido evergeen <dicksonevegreenido@gmail.com>
2023-03-04 23:55:57 +0100

    add welcome screen

--- app/i18n/en.ts
+++ app/i18n/en.ts
@@ -5,10 +5,12 @@ const en = {
     back: "Back",
   },
   welcomeScreen: {
+    logo: `Insta\nValley`,
     postscript:
       "psst  — This probably isn't what your app looks like. (Unless your designer handed you these screens, and in that case, ship it!)",
     readyForLaunch: "Your app, almost ready for launch!",
     exciting: "(ohh, this is exciting!)",
+
   },
   errorScreen: {
     title: "Something went wrong!",
